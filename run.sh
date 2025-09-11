c59511a472374860ac30eb2f4ce9cce3937b8402
Kirdow <10155603+Kirdow@users.noreply.github.com>
2025-09-11 16:45:52 +0200

    chore: improve run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-while true; do
-    read -p "Please enter episode: " number
-    if [[ $number =~ ^[0-9]+$ ]]; then
-        break
-    else
-        echo "Invalid input. Please enter a valid number."
-    fi
-done
+if [[ $# -eq 0 || ! $1 =~ ^[0-9]+$ ]]; then
+    while true; do
+        read -p "Please enter episode: " number
+        if [[ $number =~ ^[0-9]+$ ]]; then
+            break
+        else
+            echo "Invalid input. Please enter a valid number."
+        fi
+    done
+else
+    number="$1"
+fi
 
-FILE=./cmd/ep${number}/main.go
+echo "Running Episode #$number!"
 
-if [ ! -f "$FILE" ]; then
-    echo "File not found!"
+DIR=./cmd/ep${number}
+MAIN=$DIR/main.go
+
+if [ ! -d "$DIR" ]; then
+    echo "Episode not found!"
+    exit
+fi
+
+if [ ! -f "$MAIN" ]; then
+    echo "main.go for #$number not found!"
     exit
 fi
 
 set -e
 
 echo "] Building"
-time go build "$FILE"
+time go build -o main "$DIR"
 
 echo "./main"
 ./main
+
